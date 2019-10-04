b3740a525962fceb5dafa99300049cb95f0f954f
seeren <consultant@seeren.fr>
2019-10-04 03:15:10 +0200

    Add cordova support

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import { AppComponent } from "./app/app.component";
 import { RouterComponent } from "babel-skeleton";
+import { AppComponent } from "./app/app.component";
 import { LoaderComponent } from "./app/loader/loader.component";
 import { SquareListComponent } from "./app/squares/square-list/square-list.component";
 
-window.onload = () => {
-    RouterComponent
-        .add("/", "loader", LoaderComponent)
-        .add("/squares", "square-list", SquareListComponent)
-        .run(new AppComponent)
-}
+(run => window.cordova
+    ? window.document.addEventListener("deviceready", run, false)
+    : window.addEventListener("load", run, false)
+)(() => RouterComponent
+    .add("/", "loader", LoaderComponent)
+    .add("/squares", "square-list", SquareListComponent)
+    .run(new AppComponent)
+);
