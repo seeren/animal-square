96b50d61fcec41941465aa35f76e7b498c98b115
seeren <consultant@seeren.fr>
2019-10-23 23:51:16 +0200

    Route error component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { RouterComponent } from "babel-skeleton";
 import { AppComponent } from "./app/app.component";
 import { LoaderComponent } from "./app/loader/loader.component";
 import { SquareListComponent } from "./app/squares/square-list/square-list.component";
+import { ErrorComponent } from "./app/error/error.component";
 
 (run => window.cordova
     ? window.document.addEventListener("deviceready", run, false)
@@ -9,5 +10,6 @@ import { SquareListComponent } from "./app/squares/square-list/square-list.compo
 )(() => RouterComponent
     .add("/", "loader", LoaderComponent)
     .add("/squares", "square-list", SquareListComponent)
+    .add("/error", "error", ErrorComponent)
     .run(new AppComponent)
 );
