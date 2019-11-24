ca4c9ff327cf838d554de409877d061b04f0034b
seeren <consultant@seeren.fr>
2019-11-24 17:03:34 +0100

    Add square component

---
+++ src/app/squares/square-list/square/square.component.js
@@ -0,0 +1,14 @@
+import { Component } from "babel-skeleton";
+import { template } from "./square.component.html";
+
+export class SquareComponent extends Component {
+
+    constructor(square) {
+        super({
+            selector: "square",
+            template: template
+        });
+        this.square = square;
+    }
+
+}
