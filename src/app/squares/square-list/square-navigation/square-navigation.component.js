ebf1bf8ccf7b0e09be450126658400a014f418b8
seeren <consultant@seeren.fr>
2019-11-24 16:02:11 +0100

    Create square navigation

---
+++ src/app/squares/square-list/square-navigation/square-navigation.component.js
@@ -0,0 +1,23 @@
+import { Component } from "babel-skeleton";
+import { template } from "./square-navigation.component.html";
+import { SquareService } from "../../../../shared/services/square.service";
+import { SquareListService } from "../../../../shared/services/square-list.service";
+
+export class SquareNavigationComponent extends Component {
+
+    constructor() {
+        super({
+            selector: "square-navigation",
+            template: template
+        });
+        this.square = SquareService.get();
+        this.squaresLength = SquareListService.get().length;
+    }
+
+    slide(offset) {       
+        return SquareService.set(this.square = SquareListService.get().find(
+            (square) => square.level.number === this.square.level.number + offset
+        ));
+    }
+
+}
