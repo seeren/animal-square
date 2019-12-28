24dceea60aac68eb311c2c1a8fc99740d4ee2dca
seeren <consultant@seeren.fr>
2019-12-28 17:54:05 +0100

    Add swap compability

--- src/app/squares/square-list/square-navigation/square-navigation.component.js
+++ src/app/squares/square-list/square-navigation/square-navigation.component.js
@@ -1,7 +1,8 @@
 import { Component } from "babel-skeleton";
+
 import { template } from "./square-navigation.component.html";
-import { SquareService } from "../../../shared/services/square.service";
-import { SquareListService } from "../../../shared/services/square-list.service";
+import { SquareService } from "../../shared/services/square.service";
+import { SquareListService } from "../../shared/services/square-list.service";
 
 export class SquareNavigationComponent extends Component {
 
@@ -10,13 +11,39 @@ export class SquareNavigationComponent extends Component {
             selector: "square-navigation",
             template: template
         });
+        SquareService.attach(service => this.onSquare(service.get()));
+    }
+
+    onInit() {
+        this.id = this.id || 0;
         this.square = SquareService.get();
-        this.squaresLength = SquareListService.get().length;
+        this.next = SquareListService.isNext(this.square);
+        this.previous = SquareListService.isPrevious(this.square);
+    }
+
+    onUpdate(element) {
+        this.id = window.clearTimeout(this.id);
+        const links = element.getElementsByTagName("a");
+        const linksOnClick = [];
+        for (const link of links) {
+            linksOnClick.push(link.onclick);
+            link.onclick = null;
+        }
+        this.id = window.setTimeout(() => linksOnClick.forEach(
+            (onClick, key) => links[key].onclick = onClick
+        ), 500);
+    }
+
+    onSquare(square) {
+        if (this.square !== square) {
+            this.onInit();
+            this.update();
+        }
     }
 
     slide(offset) {
-        return SquareService.set(this.square = SquareListService.get().find(
-            square => square.level.number === this.square.level.number + offset
+        SquareService.set(SquareListService.get().find(square =>
+            square.level.number === this.square.level.number + offset
         ));
     }
 
