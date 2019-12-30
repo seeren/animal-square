70428772284c923f9e5d341d4b868b5f3d27400a
seeren <consultant@seeren.fr>
2019-12-30 17:33:28 +0100

    Work on hotreload

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import { RouterComponent } from "babel-skeleton";
-import { AppComponent } from "./app/app.component";
-import { LoaderComponent } from "./app/loader/loader.component";
-import { SquareListComponent } from "./app/squares/square-list/square-list.component";
-import { ErrorComponent } from "./app/error/error.component";
+// import { RouterComponent } from "babel-skeleton";
 
-(run => window.cordova
-    ? window.document.addEventListener("deviceready", run, false)
-    : window.addEventListener("load", run, false)
-)(() => RouterComponent
-    .add("/", "loader", LoaderComponent)
-    .add("/squares", "square-list", SquareListComponent)
-    .add("/error", "error", ErrorComponent)
-    .run(new AppComponent)
-);
+// import { AppComponent } from "./app/app.component";
+// import { LoaderComponent } from "./app/loader/loader.component";
+// import { SquareListComponent } from "./app/square-list/square-list.component";
+// import { SquarePuzzleComponent } from "./app/square-puzzle/square-puzzle.component";
+// import { ErrorComponent } from "./app/error/error.component";
+// import { ErrorService } from "./app/error/shared/services/error.service";
+
+// (run => window.cordova
+//     ? window.document.addEventListener("deviceready", run, false)
+//     : window.addEventListener("load", run, false)
+// )(() => {
+//     try {
+//         RouterComponent
+//             .add("/", "loader", LoaderComponent)
+//             .add("/squares", "square-list", SquareListComponent)
+//             // .add("/", "square-puzzle", SquarePuzzleComponent)
+//             // .add("/squares/:id", "puzzle", SquarePuzzleComponent)
+//             .add("/error", "error", ErrorComponent)
+//             .run(new AppComponent)
+//     } catch (error) {
+//         ErrorService.set(error);
+//     }
+// });
+
+console.log("log");
