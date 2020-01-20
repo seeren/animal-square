2b545f0cbc0d1c4f8ec664273f1f3ac12e80e76e
seeren <consultant@seeren.fr>
2020-01-20 23:06:09 +0100

    Implement click event

--- src/app/square-puzzle/puzzle/puzzle.component.js
+++ src/app/square-puzzle/puzzle/puzzle.component.js
@@ -1,9 +1,13 @@
 import { Component } from "babel-skeleton";
 import { template } from "./puzzle.component.html";
 import { SquareService } from "../../shared/services/square.service";
+import { DirectionService } from "../../shared/services/direction.service";
 
 export class PuzzleComponent extends Component {
 
+    /**
+     * @constructor
+     */
     constructor() {
         super({
             selector: "puzzle",
@@ -11,8 +15,47 @@ export class PuzzleComponent extends Component {
         });
     }
 
+    /**
+     * @fires
+     */
     onInit() {
         this.square = SquareService.get();
+    }
+
+    /**
+     * @fires
+     */
+    onUpdate() {
+        for (const cel of window.document.querySelectorAll(`${this.selector} .cel`)) {
+            cel.onclick = (event) => this.onClick(event);
+        }
+    }
+
+    /**
+     * @event
+     * @param {MouseEvent} event 
+     */
+    onClick(event) {
+        const direction = DirectionService.get(event);
+        if (!direction.property) {
+            return;
+        }
+        const cel = event.target;
+        cel.style[direction.property] = `${parseInt(
+            window.getComputedStyle(cel).getPropertyValue(direction.property), 10
+        ) + direction.distance}px`;
+    }
+
+    onTouchStart() {
+
+    }
+
+    onTouchMove() {
+
+    }
+
+    onTouchEnd() {
+
     }
 
 }
