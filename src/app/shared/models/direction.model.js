c5541e6efa07046c8443ca73f718c0905f788811
seeren <consultant@seeren.fr>
2020-01-23 15:29:25 +0100

    Add axe and sign

--- src/app/shared/models/direction.model.js
+++ src/app/shared/models/direction.model.js
@@ -4,8 +4,10 @@ export class Direction {
      * @constructor
      */
     constructor() {
+        this.axe = null;
         this.property = null;
         this.distance = null
+        this.positive = null;
     }
 
 
