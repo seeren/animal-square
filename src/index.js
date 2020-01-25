f2c7851ff7deb90ba555f09c51df8549284ccdd6
seeren <consultant@seeren.fr>
2020-01-26 00:57:40 +0100

    Uncomment

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-// import { RouterComponent } from "babel-skeleton";
+import { RouterComponent } from "babel-skeleton";
 
-// import { AppComponent } from "./app/app.component";
-// import { LoaderComponent } from "./app/loader/loader.component";
-// import { SquareListComponent } from "./app/square-list/square-list.component";
-// import { SquarePuzzleComponent } from "./app/square-puzzle/square-puzzle.component";
-// import { ErrorComponent } from "./app/error/error.component";
-// import { ErrorService } from "./app/error/shared/services/error.service";
+import { AppComponent } from "./app/app.component";
+import { LoaderComponent } from "./app/loader/loader.component";
+import { SquareListComponent } from "./app/square-list/square-list.component";
+import { SquarePuzzleComponent } from "./app/square-puzzle/square-puzzle.component";
+import { ErrorComponent } from "./app/error/error.component";
+import { ErrorService } from "./app/error/shared/services/error.service";
 
-// (run => window.cordova
-//     ? window.document.addEventListener("deviceready", run, false)
-//     : window.addEventListener("load", run, false)
-// )(() => {
-//     try {
-//         RouterComponent
-//             .add("/", "loader", LoaderComponent)
-//             .add("/squares", "square-list", SquareListComponent)
-//             // .add("/", "square-puzzle", SquarePuzzleComponent)
-//             // .add("/squares/:id", "puzzle", SquarePuzzleComponent)
-//             .add("/error", "error", ErrorComponent)
-//             .run(new AppComponent)
-//     } catch (error) {
-//         ErrorService.set(error);
-//     }
-// });
-
-console.log("log");
+(run => window.cordova
+    ? window.document.addEventListener("deviceready", run, false)
+    : window.addEventListener("load", run, false)
+)(() => {
+    try {
+        RouterComponent
+            .add("/", "loader", LoaderComponent)
+            .add("/error", "error", ErrorComponent)
+            .add("/squares", "square-list", SquareListComponent)
+            .add("/squares/:id", "square-puzzle", SquarePuzzleComponent)
+            .run(new AppComponent)
+    } catch (error) {
+        ErrorService.set(error);
+    }
+});
