64c41d77ef6f717e05b5d8746a55c891bcda2656
seeren <consultant@seeren.fr>
2020-02-12 12:27:24 +0100

    Use elements from point

--- src/app/shared/services/direction.service.js
+++ src/app/shared/services/direction.service.js
@@ -12,28 +12,52 @@ export const DirectionService = new class extends Service {
 
     /**
      * @param {TouchEvent} event 
+     * @returns {Direction[]}
+     */
+    getAll(event) {
+        const top = new Direction;
+        const bottom = new Direction;
+        const right = new Direction;
+        const left = new Direction;
+        top.x = event.touches[0].clientX;
+        top.y = event.touches[0].clientY - event.target.clientHeight;
+        top.property = "top";
+        top.axe = "Y";
+        top.positive = false;
+        bottom.x = event.touches[0].clientX;
+        bottom.y = event.touches[0].clientY + event.target.clientHeight;
+        bottom.property = "top";
+        bottom.axe = "Y";
+        bottom.positive = true;
+        right.x = event.touches[0].clientX + event.target.clientWidth;
+        right.y = event.touches[0].clientY;
+        right.property = "left";
+        right.axe = "X";
+        right.positive = true;
+        left.x = event.touches[0].clientX - event.target.clientWidth;
+        left.y = event.touches[0].clientY;
+        left.property = "left";
+        left.axe = "X";
+        left.positive = false;
+        return [top, bottom, right, left];
+    }
+
+    /**
+     * @param {TouchEvent} event 
      * @returns {Direction}
      */
     get(event) {
-        const direction = new Direction;
-        if (event.target.parentNode === window.document.elementFromPoint(event.touches[0].clientX, event.touches[0].clientY - event.target.clientHeight)) {
-            direction.property = "top";
-            direction.axe = "Y";
-            direction.positive = false;
-        } else if (event.target.parentNode === window.document.elementFromPoint(event.touches[0].clientX, event.touches[0].clientY + event.target.clientHeight)) {
-            direction.property = "top";
-            direction.axe = "Y";
-            direction.positive = true;
-        } else if (event.target.parentNode === window.document.elementFromPoint(event.touches[0].clientX + event.target.clientWidth, event.touches[0].clientY)) {
-            direction.property = "left";
-            direction.axe = "X";
-            direction.positive = true;
-        } else if (event.target.parentNode === window.document.elementFromPoint(event.touches[0].clientX - event.target.clientWidth, event.touches[0].clientY)) {
-            direction.property = "left";
-            direction.axe = "X";
-            direction.positive = false;
+        for (const direction of this.getAll(event)) {
+            const elements = document.elementsFromPoint(direction.x, direction.y);
+            const index = elements.indexOf(event.target.parentNode);
+            if (-1 !== index) {
+                const element = elements[index - 1];
+                if (!element || element.tagName !== event.target.tagName) {
+                    return direction;
+                }
+            }
         }
-        return direction;
+        return new Direction;
     }
 
 }
