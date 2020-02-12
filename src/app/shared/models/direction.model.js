07dec2a8d8492943a40be8b611d29c370da16a1e
seeren <consultant@seeren.fr>
2020-02-12 12:24:02 +0100

    Add x and y properties

--- src/app/shared/models/direction.model.js
+++ src/app/shared/models/direction.model.js
@@ -4,6 +4,8 @@ export class Direction {
      * @constructor
      */
     constructor() {
+        this.x = null;
+        this.y = null;
         this.axe = null;
         this.property = null;
         this.distance = null
