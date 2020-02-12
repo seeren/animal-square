b0d407111d10badf2eb0724ae485a775a6d4e9ec
seeren <consultant@seeren.fr>
2020-02-12 12:27:53 +0100

    Add start stop and hit

--- src/app/shared/services/monkey.service.js
+++ src/app/shared/services/monkey.service.js
@@ -4,6 +4,19 @@ export const MonkeyService = new class extends Service {
 
     constructor() {
         super();
+        this.state = null;
+    }
+
+    start() {
+        this.notify(this.state = "start");
+    }
+
+    hit() {
+        this.notify(this.state = "hit");
+    }
+
+    stop() {
+        this.notify(this.state = "stop");
     }
 
 }
