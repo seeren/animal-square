d6d76b88fa1fc47a186f74b461149c4b4ebb9613
seeren <consultant@seeren.fr>
2020-02-18 11:26:07 +0100

    Create puzzle service

---
+++ src/app/shared/services/puzzle.service.js
@@ -0,0 +1,47 @@
+import { Service } from 'babel-skeleton';
+
+export const PuzzleService = new class extends Service {
+
+    constructor() {
+        super();
+    }
+
+    /**
+     * @param {HTMLElement}
+     * @returns {Number}
+     */
+    duration(cel) {
+        return window.parseFloat(
+            window.getComputedStyle(cel).getPropertyValue("transition-duration"),
+            10
+        );
+    }
+
+    /**
+     * @returns {Boolean}
+     */
+    complete() {
+        try {
+            const width = window.document.querySelector(`puzzle .cel`).offsetWidth;
+            window.document.querySelectorAll(`puzzle .cel`).forEach((element, index) => {
+                const offsetTop = width * window.parseInt(index / 4);
+                const offsetLeft = width * (index % 4);
+                if (element.offsetTop !== offsetTop
+                    && (element.offsetTop > offsetTop + 5
+                        || element.offsetTop < offsetTop - 5)) {
+                    throw element;
+                }
+                if (element.offsetLeft !== offsetLeft
+                    && (element.offsetLeft > offsetLeft + 5
+                        || element.offsetLeft < offsetLeft - 5)) {
+                    throw element;
+                }
+            });
+        } catch (element) {
+            return false;
+        }
+        return true;
+    }
+
+
+}
