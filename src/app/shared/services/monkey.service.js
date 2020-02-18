102e9aa96abce716c27ea6a578103c0622ec459b
seeren <consultant@seeren.fr>
2020-02-18 11:25:36 +0100

    Add getters for number random and cel

--- src/app/shared/services/monkey.service.js
+++ src/app/shared/services/monkey.service.js
@@ -1,32 +1,82 @@
 import { Service } from 'babel-skeleton';
+import { ScoreService } from './score.service';
+import { ResumeService } from './resume.service';
 
 export const MonkeyService = new class extends Service {
 
+    /**
+     * @constructor
+     */
     constructor() {
         super();
         this.state = null;
         this.number = null;
     }
 
+    /**
+     * @returns {Boolean}
+     */
+    isPlayable() {
+        return !ResumeService.resume && "stop" !== ScoreService.state;
+    }
+
+    /**
+     * @emits
+     */
     hit() {
         this.notify(this.state = "hit");
     }
 
+    /**
+     * @emits
+     */
+    stop() {
+        this.notify(this.state = "stop");
+    }
+
+    /**
+     * @param {Number} number
+     */
     start(number) {
         this.number = number;
         this.notify(this.state = "start");
     }
 
-    stop() {
-        this.notify(this.state = "stop");
+    /**
+     * @emits
+     */
+    play() {
+        this.notify(this.state = "play");
     }
 
+    /**
+     * @emits
+     */
     pause() {
         this.notify(this.state = "pause");
     }
 
-    resume() {
-        this.notify(this.state = "resume");
+
+    /**
+     * @returns {Number} 
+     */
+    random() {
+        let number = this.number;
+        while (number === this.number) {
+            number = Math.floor(Math.random() * Math.floor(14)) + 1;
+        }
+        return number;
+    }
+
+    /**
+     * @returns {HTMLElement} 
+     */
+    cel() {
+        const shape = window.document.querySelector(`monkey`).getBoundingClientRect();
+        return window.document.elementsFromPoint(
+            shape.x + 5,
+            shape.y + 5
+        ).find(element => -1 !== element.className.indexOf("cel cel-"));
     }
 
 }
