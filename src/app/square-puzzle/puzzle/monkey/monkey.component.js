04d09e6e0c6573a33f3bac61ff013dc16819311e
seeren <consultant@seeren.fr>
2020-03-22 17:34:35 +0100

    Implements pause and resume

--- src/app/square-puzzle/puzzle/monkey/monkey.component.js
+++ src/app/square-puzzle/puzzle/monkey/monkey.component.js
@@ -21,23 +21,74 @@ export class MonkeyComponent extends Component {
      * @fires
      */
     onInit() {
-        this.timeout = 0
+        this.monkey = null;
+        this.duration = 0;
+        this.delay = 0;
+        this.interval = 0;
         MonkeyService.attach(this.listener);
-    }
-    /**
-     * @fires
-     */
-    onUpdate(element) {
-        this.monkey = element;
-        this.duration = window.parseFloat(window.getComputedStyle(element).getPropertyValue("animation-duration"), 10) * 1000
     }
 
     /**
      * @fires
      */
     onDestroy() {
-        this.timeout = window.clearTimeout(this.timeout);
+        window.clearInterval(this.interval);
         MonkeyService.detach(this.listener);
+    }
+
+    /**
+     * @fires
+     */
+    onUpdate(element) {
+        this.monkey = element;
+        this.duration = window.parseFloat(
+            window.getComputedStyle(element).getPropertyValue("animation-duration"), 10
+        ) * 1000;
+        this.delay = this.duration / 2;
+    }
+
+    /**
+     * @fires
+     */
+    onPause() {
+        if (this.delay !== this.duration / 2) {
+            window.clearInterval(this.interval);
+            this.monkey.className += " pause";
+        }
+    }
+
+    /**
+     * @fires
+     */
+    onResume() {
+        if (-1 !== this.monkey.className.indexOf(" pause")) {
+            this.monkey.className = this.monkey.className.replace(" pause", "");
+            this.interval = this.runInterval();
+        }
+    }
+
+    /**
+     * @return {Number}
+     */
+    runInterval() {
+        return window.setInterval(() => {
+            this.delay -= 100;
+            if (0 === this.delay) {
+                window.clearInterval(this.interval);
+                MonkeySoundService.hit();
+                MonkeyService.hit();
+                this.delay = this.duration / 2;
+            }
+        }, 100);
+    }
+
+    /**
+     * @event
+     */
+    start() {
+        MonkeySoundService.start();
+        this.monkey.className = `monkey-${MonkeyService.number}`;
+        this.interval = this.runInterval();
     }
 
     /**
@@ -50,30 +101,15 @@ export class MonkeyComponent extends Component {
     /**
      * @event
      */
-    start() {
-        MonkeySoundService.start();
-        this.play();
-        this.monkey.className = `monkey-${MonkeyService.number}`;
-        this.timeout = window.setTimeout(() => {
-            MonkeySoundService.hit();
-            MonkeyService.hit();
-        }, this.duration / 2);
-    }
-
-    /**
-     * @event
-     */
     play() {
-        this.monkey.className = this.monkey.className.replace(" pause", "");
+        this.onResume();
     }
 
     /**
      * @event
      */
     pause() {
-        if (-1 === this.monkey.className.indexOf(" pause")) {
-            this.monkey.className += " pause";
-        }
+        this.onPause();
     }
 
 }
