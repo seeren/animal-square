e8dfe5f30e0a2a4c816550ad44517e9d6d5073f9
seeren <consultant@seeren.fr>
2020-03-26 14:30:14 +0100

    Use identifier instead of key

--- src/app/shared/services/sounds/jungle-sound.service.js
+++ src/app/shared/services/sounds/jungle-sound.service.js
@@ -15,8 +15,8 @@ export const JungleSoundService = new class extends Service {
      * @returns {void} 
      */
     puzzle() {
-        !SoundService.players["jungle"]
-            || -1 === SoundService.players["jungle"].src.indexOf("puzzle")
+        !SoundService.players.jungle
+            || -1 === SoundService.players.jungle.src.indexOf("puzzle")
             ? SoundService.play("jungle", `puzzle.mp4`, true).volume = 1
             : SoundService.get("jungle").play();
     }
