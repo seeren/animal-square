fe8034a2a11c25f5e8c47597bc27d1c9415d8a69
seeren <consultant@seeren.fr>
2021-09-06 12:41:48 +0200

    Trigger audio load

--- src/app/loader/loader.component.js
+++ src/app/loader/loader.component.js
@@ -1,6 +1,8 @@
 import { Component, RouterComponent } from "babel-skeleton";
 
+import { default as game } from './../../../config/game.json';
 import { template } from "./loader.component.html";
+
 import { LoaderError } from "./shared/loader.error";
 import { SquareListService } from "../shared/services/square-list.service";
 import { ErrorService } from "../error/shared/error.service";
@@ -8,9 +10,6 @@ import { BirdSoundService } from "../shared/services/sounds/bird-sound.service";
 
 export class LoaderComponent extends Component {
 
-    /**
-     * @constructor
-     */
     constructor() {
         super({
             selector: "loader",
@@ -21,41 +20,18 @@ export class LoaderComponent extends Component {
         this.audios = this.getAudios();
     }
 
-    /**
-     * @fires
-     */
     onInit() {
         if (!this.done) {
             window.setTimeout(() => {
-                this.images.forEach(image => this.getImage().src = `dist/assets/images/${image}`);
-                this.audios.forEach(audio => this.getAudio().src = `dist/assets/mp4/${audio}`);
+                this.images.forEach((image) => this.getImage(`dist/assets/images/${image}`));
+                this.audios.forEach((audio) => this.getAudio(`dist/assets/mp4/${audio}`));
             }, 1000);
         }
     }
 
-    /**
-     * @returns {String[]}
-     */
     getImages() {
-        const images = [
-            "items/fires/fire-down-left.png",
-            "items/fires/fire-down-right.png",
-            "items/fires/fire-left-down.png",
-            "items/fires/fire-left-up.png",
-            "items/fires/fire-right-down.png",
-            "items/fires/fire-right-up.png",
-            "items/fires/fire-up-left.png",
-            "items/fires/fire-up-right.png",
-            "items/medails/medail-black.png",
-            "items/medails/medail-bronze.png",
-            "items/medails/medail-gold.png",
-            "items/medails/medail-silver.png",
-            "items/monkeys/monkey-right.png",
-            "items/navigations/navigation-next.png",
-            "items/navigations/navigation-previous.png",
-            "items/navigations/navigation-previous.png",
-        ];
-        SquareListService.get().forEach(square => {
+        const images = game.images;
+        SquareListService.get().forEach((square) => {
             const basePath = `animals/${square.animal.name}/${square.animal.name}`;
             images.push(
                 `${basePath}-background.jpg`,
@@ -69,69 +45,39 @@ export class LoaderComponent extends Component {
         });
         return images;
     }
+    
+    getAudios() {
+        return game.sounds;
+    }
 
-    /**
-     * @returns {Image}
-     */
-    getImage() {
+    getImage(src) {
         const image = new Image;
         image.onerror = () => this.onError(image.src);
         image.onload = () => this.onLoad();
-        return image;
+        image.src = src;
     }
 
-    /**
-     * @returns {String[]}
-     */
-    getAudios() {
-        return [
-            "bird-fail.mp4",
-            "bird-signal.mp4",
-            "bird-success.mp4",
-            "magic.mp4",
-            "monkey-hit.mp4",
-            "monkey-start.mp4",
-            "page.mp4",
-            "puzzle.mp4",
-            "square.mp4",
-            "visit.mp4",
-            "whip.mp4",
-        ];
-    }
-
-    /**
-     * @returns {Audio}
-     */
-    getAudio() {
+    getAudio(src) {
         const audio = new Audio;
         audio.onerror = () => this.onError(audio.src);
         audio.oncanplaythrough = () => this.onLoad();
-        return audio;
+        audio.src = src;
+        audio.load();
     }
 
-    /**
-     * @event
-     */
     onLoad() {
         if (!ErrorService.get()) {
             this.done = (this.done + (
                 100 / (this.images.length + this.audios.length) * 100 / 100
             )) * 100 / 100;
             this.update();
-        }
+        } 
     }
 
-    /**
-     * @event
-     * @param {String} src 
-     */
     onError(src) {
         ErrorService.set(new LoaderError(src));
     }
 
-    /**
-     * @event
-     */
     visit() {
         BirdSoundService.signal();
         RouterComponent.navigate("square-list");
