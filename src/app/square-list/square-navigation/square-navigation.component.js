704522e65771e030d41388a0f401ce3791e5626d
seeren <consultant@seeren.fr>
2021-09-06 16:33:54 +0200

    Refactor

--- src/app/square-list/square-navigation/square-navigation.component.js
+++ src/app/square-list/square-navigation/square-navigation.component.js
@@ -1,46 +1,31 @@
-import { Component, RouterComponent } from "babel-skeleton";
+import { Component, RouterComponent } from 'babel-skeleton';
 
-import { template } from "./square-navigation.component.html";
-import { SquareService } from "../../shared/services/square.service";
-import { SquareListService } from "../../shared/services/square-list.service";
-import { Square } from "../../shared/models/square.model";
-import { PageSoundService } from "../../shared/services/sounds/page-sound.service";
+import { template } from './square-navigation.component.html';
+import { SquareService } from '../../shared/services/square.service';
+import { SquareListService } from '../../shared/services/square-list.service';
+import { PageSoundService } from '../../shared/services/sounds/page-sound.service';
 
 export class SquareNavigationComponent extends Component {
 
-    /**
-     * @constructor
-     */
     constructor() {
-        super({
-            selector: "square-navigation",
-            template: template
-        });
+        super({ selector: 'square-navigation', template });
         this.listner = service => this.onSquare(service.get());
     }
 
-    /**
-     * @fires
-     */
     onInit() {
         this.id = this.id || 0;
         this.square = SquareService.get();
-        this.next = SquareListService.isNext(this.square);
-        this.previous = SquareListService.isPrevious(this.square);
+        this.next = SquareListService.hasNext(this.square);
+        console.log(this.next);
+        this.previous = SquareListService.hasPrevious(this.square);
         this.length = SquareListService.get().length;
         SquareService.attach(this.listner);
     }
 
-    /**
-     * @fires
-     */
     onDestroy() {
         SquareService.detach(this.listner);
     }
 
-    /**
-     * @fires
-     */
     onUpdate() {
         this.id = window.clearTimeout(this.id);
         const links = window.document.querySelectorAll(`${this.selector} a`);
@@ -49,15 +34,12 @@ export class SquareNavigationComponent extends Component {
             linksOnClick.push(link.onclick);
             link.onclick = null;
         }
-        this.id = window.setTimeout(() => linksOnClick.forEach(
-            (onClick, key) => links[key].onclick = onClick
-        ), 500);
+        this.id = window.setTimeout(
+            () => linksOnClick.forEach((onClick, key) => links[key].onclick = onClick),
+            500
+        );
     }
 
-    /**
-     * @event
-     * @param {Square} square 
-     */
     onSquare(square) {
         if (this.square !== square) {
             this.onInit();
@@ -65,22 +47,16 @@ export class SquareNavigationComponent extends Component {
         }
     }
 
-    /**
-     * @event
-     * @param {Number} offset 
-     */
     slide(offset) {
+        console.log('slide');
         PageSoundService.play();
         SquareService.set(SquareListService.get().find(square =>
             square.level.number === this.square.level.number + offset
         ));
     }
 
-    /**
-     * @event
-     */
     ranking() {
-        RouterComponent.navigate("ranking");
+        RouterComponent.navigate('ranking');
     }
 
 }
