326d1f539882e73baa8ddb289a30fdb15c90bda7
seeren <consultant@seeren.fr>
2021-09-08 23:13:40 +0200

    Trigger notice on content instead of container

--- src/app/shared/components/notice/notice.component.js
+++ src/app/shared/components/notice/notice.component.js
@@ -1,128 +1,86 @@
-import { Component } from "babel-skeleton";
+import { Component } from 'babel-skeleton';
 
-import { template } from "./notice.component.html";
-import { Notice } from "../../models/notice.model";
+import { template } from './notice.component.html';
+
+import { Notice } from '../../models/notice.model';
 
 export class NoticeComponent extends Component {
 
-    /**
-     * @constructor
-     */
     constructor() {
-        super({
-            selector: "notice",
-            template: template
-        });
+        super({ selector: 'notice', template });
         this.notice = new Notice;
         this.element = null;
+        this.onclick = null;
     }
 
-    /**
-     * @param {String} title
-     */
     set title(title) {
         this.notice.title = title;
     }
 
-    /**
-     * @returns {String}
-     */
     get title() {
         return this.notice.title;
     }
 
-    /**
-     * @param {String} background
-     */
     set background(background) {
         this.notice.background = background;
     }
 
-    /**
-     * @fires
-     */
     onInit() {
-        this.title = this.notice.title || "Notice";
+        this.title = this.notice.title || 'Notice';
         this.background = null;
     }
 
-    /**
-     * 
-     * @fires
-     * @param {HTMLElement} element 
-     */
     onUpdate(element) {
         this.element = element;
         if (this.notice.background) {
-            this.element.style.backgroundImage = `url("dist/assets/images/${
-                this.notice.background
-                }")`;
+            this.element.style.backgroundImage = `url('dist/assets/images/${this.notice.background}')`;
         }
     }
 
-    /**
-      * @fires
-      */
     onPause() {
-        if (-1 === this.element.className.indexOf(" pause")) {
-            this.element.className += " pause";
+        if (-1 === this.element.className.indexOf(' pause')) {
+            this.element.className += ' pause';
         }
     }
 
-    /**
-     * @fires
-     */
     onResume() {
-        if (-1 !== this.element.className.indexOf(" pause")) {
-            this.element.className = this.element.className.replace(" pause", "");
+        if (-1 !== this.element.className.indexOf(' pause')) {
+            this.element.className = this.element.className.replace(' pause', '');
         }
     }
 
-    /**
-     * @param {String} state
-     * @return {Number}
-     */
+    click() {
+        if (this.onclick) {
+            this.onclick();
+        }
+    }
+
     animate(state, text) {
         if (text) {
             this.title = text;
-            this.animate("hide");
+            this.animate('hide');
             this.update();
         }
         this.element.className = `animate animate-${state}`;
         return window.parseFloat(
-            window.getComputedStyle(this.element).getPropertyValue("animation-duration")
+            window.getComputedStyle(this.element).getPropertyValue('animation-duration')
         ) * 1000;
     }
 
-    /**
-     * @param {String} text
-     * @return {Number}
-     */
     show(text) {
-        return this.animate("show", text || this.title);
+        return this.animate('show', text || this.title);
     }
 
-    /**
-     * @return {Number}
-     */
     hide() {
-        return this.animate("hide");
+        return this.animate('hide');
     }
 
-    /**
-     * @param {String} text
-     * @return {Number}
-     */
     scroll(text) {
-        return this.animate("scroll", text || this.title);
+        return this.animate('scroll', text || this.title);
     }
 
-    /**
-     * @param {String} text
-     * @return {Number}
-     */
     pass(text) {
-        return this.animate("pass", text || this.title);
+        return this.animate('pass', text || this.title);
     }
 
 }
--- src/app/square-list/square/square.component.js
+++ src/app/square-list/square/square.component.js
@@ -1,76 +1,53 @@
-import { Component, RouterComponent } from "babel-skeleton";
+import { Component, RouterComponent } from 'babel-skeleton';
 
-import { template } from "./square.component.html";
-import { Square } from "../../shared/models/square.model";
-import { NoticeComponent } from "../../shared/components/notice/notice.component";
-import { ScoreService } from "../../shared/services/score.service";
-import { WhipSoundService } from "../../shared/services/sounds/whip-sound.service";
+import { template } from './square.component.html';
+
+import { NoticeComponent } from '../../shared/components/notice/notice.component';
+import { ScoreService } from '../../shared/services/score.service';
+import { WhipSoundService } from '../../shared/services/sounds/whip-sound.service';
 
 export class SquareComponent extends Component {
 
-    /**
-     * @constructor
-     * @param {Square} square 
-     */
     constructor(square) {
-        super({
-            selector: "square",
-            template: template,
-            components: [
+        super({ selector: 'square', template, components: [
                 new NoticeComponent
             ]
         });
         this.square = square;
         this.notice = this.components[0];
+        this.notice.onclick = () => this.visit();
     }
 
-    /**
-     * @fires
-     */
     onInit() {
         this.timeout = null;
         this.medail = ScoreService.medail(this.square.score.time);
     }
 
-    /**
-     * @fires
-     */
     onDestroy() {
         this.onPause();
     }
 
-    /**
-     * @fires
-     */
     onUpdate() {
         this.notice.background = `animals/${
             this.square.animal.name}/${
             this.square.animal.name}-${
-            this.square.score.time ? `color` : `black`}.png`;
-        this.notice.show("Visit");
+            this.square.score.time ? `color` : `black`
+        }.png`;
+        this.notice.show('Visit');
     }
 
-    /**
-     * @fires
-     */
     onPause() {
         if (this.timeout) {
             window.clearTimeout(this.timeout);
         }
     }
 
-    /**
-     * @fires
-     */
     onResume() {
         if (this.timeout) {
             this.navigate(500);
         }
     }
 
-    /**
-     * @event
-     */
     visit() {
         if (!this.timeout) {
             WhipSoundService.play();
@@ -78,15 +55,9 @@ export class SquareComponent extends Component {
         }
     }
 
-    /**
-     * @event
-     * @param {Number}
-     */
     navigate(duration) {
         this.timeout = window.setTimeout(
-            () => RouterComponent.navigate("square-puzzle", {
-                id: this.square.level.number
-            }),
+            () => RouterComponent.navigate('square-puzzle', { id: this.square.level.number }),
             duration
         );
     }
