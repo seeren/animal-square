c5cb41c3686ce39d35e7bb3b5b11564713d8ab71
seeren <consultant@seeren.fr>
2021-09-12 12:39:15 +0200

    Refactor

--- src/app/shared/services/sounds/bird-sound.service.js
+++ src/app/shared/services/sounds/bird-sound.service.js
@@ -4,37 +4,22 @@ import { SoundService } from './sound.service';
 
 export const BirdSoundService = new class extends Service {
 
-    /**
-     * @returns {void} 
-     */
     signal() {
         SoundService.play("bird", `bird-signal.mp4`);
     }
 
-    /**
-     * @returns {void} 
-     */
     success() {
         SoundService.play("bird", `bird-success.mp4`, true).volume = .7;
     }
 
-    /**
-     * @returns {void} 
-     */
     fail() {
         SoundService.play("bird", `bird-fail.mp4`, true).volume = 1;
     }
 
-    /**
-     * @returns {void} 
-     */
     pause() {
         SoundService.pause("bird");
     }
 
-    /**
-     * @returns {void} 
-     */
     resume() {
         if (SoundService.players.bird) {
             -1 === SoundService.players.bird.src.indexOf("success")
