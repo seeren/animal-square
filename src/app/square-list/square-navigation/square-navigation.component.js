cd6f2d221788fe4861d2debf93bc50ee48f5b6d8
seeren <consultant@seeren.fr>
2021-09-12 12:39:57 +0200

    Remove wait before navigate again

--- src/app/square-list/square-navigation/square-navigation.component.js
+++ src/app/square-list/square-navigation/square-navigation.component.js
@@ -1,6 +1,7 @@
 import { Component, RouterComponent } from 'babel-skeleton';
 
 import { template } from './square-navigation.component.html';
+
 import { SquareService } from '../../shared/services/square.service';
 import { SquareListService } from '../../shared/services/square-list.service';
 import { PageSoundService } from '../../shared/services/sounds/page-sound.service';
@@ -13,7 +14,6 @@ export class SquareNavigationComponent extends Component {
     }
 
     onInit() {
-        this.id = this.id || 0;
         this.square = SquareService.get();
         this.next = SquareListService.hasNext(this.square);
         this.previous = SquareListService.hasPrevious(this.square);
@@ -25,20 +25,6 @@ export class SquareNavigationComponent extends Component {
         SquareService.detach(this.listner);
     }
 
-    onUpdate() {
-        this.id = window.clearTimeout(this.id);
-        const links = window.document.querySelectorAll(`${this.selector} a`);
-        const linksOnClick = [];
-        for (const link of links) {
-            linksOnClick.push(link.onclick);
-            link.onclick = null;
-        }
-        this.id = window.setTimeout(
-            () => linksOnClick.forEach((onClick, key) => links[key].onclick = onClick),
-            500
-        );
-    }
-
     onSquare(square) {
         if (this.square !== square) {
             this.onInit();
@@ -48,9 +34,10 @@ export class SquareNavigationComponent extends Component {
 
     slide(offset) {
         PageSoundService.play();
-        SquareService.set(SquareListService.get().find(square =>
-            square.level.number === this.square.level.number + offset
-        ));
+        const square = SquareListService.get().find(
+            (square) => square.level.number === this.square.level.number + offset
+        );
+        SquareService.set(square);
     }
 
     ranking() {
