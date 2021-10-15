dc44e64eda088f33ef3509269fb69c5c65972d8c
seeren <consultant@seeren.fr>
2021-10-15 23:41:57 +0200

    Refactor

--- src/app/ranking/ranking.component.js
+++ src/app/ranking/ranking.component.js
@@ -1,86 +1,69 @@
-import { Component, RouterComponent } from "babel-skeleton";
+import { Component, RouterComponent } from 'babel-skeleton';
 
-import { template } from "./ranking.component.html";
-import { NoticeComponent } from "../shared/components/notice/notice.component";
-import { SquareListService } from "../shared/services/square-list.service";
-import { ScoreService } from "../shared/services/score.service";
-import { WhipSoundService } from "../shared/services/sounds/whip-sound.service";
-import { BirdSoundService } from "../shared/services/sounds/bird-sound.service";
+import { template } from './ranking.component.html';
+
+import { NoticeComponent } from '../shared/components/notice/notice.component';
+import { SquareListService } from '../shared/services/square-list.service';
+import { ScoreService } from '../shared/services/score.service';
+import { WhipSoundService } from '../shared/services/sounds/whip-sound.service';
+import { BirdSoundService } from '../shared/services/sounds/bird-sound.service';
 
 export class RankingComponent extends Component {
 
-    /**
-     * @constructor
-     */
     constructor() {
-        super({
-            selector: "ranking",
-            template: template,
-            components: [
+        super({ selector: 'ranking', template, components: [
                 new NoticeComponent
-            ]
-        });
+            ] });
         this.notice = this.components[0];
     }
 
-    /**
-     * @fires
-     */
     onInit() {
         let black = 0;
         let gold = 0;
         let silver = 0;
         let bronze = 0;
         this.time = 0;
-        SquareListService.get().forEach(square => {
+        SquareListService.get().forEach((square) => {
             this.time += square.score.time;
             const medail = ScoreService.medail(square.score.time);
-            "black" === medail ? black++ : ("gold" === medail
-                ? gold++ : ("silver" === medail ? silver++ : bronze++));
+            'black' === medail
+                ? black++ 
+                : ('gold' === medail
+                    ? gold++ 
+                    : ('silver' === medail
+                        ? silver++ 
+                        : bronze++));
         });
         this.medail = black > gold
-            ? "black"
-            : (gold > silver ? "gold" : (silver > bronze ? "silver" : "bronze"));
+            ? 'black'
+            : (gold > silver 
+                ? 'gold' 
+                : (silver > bronze 
+                    ? 'silver' 
+                    : 'bronze'));
     }
 
-    /**
-     * @fires
-     */
     onDestroy() {
         this.onPause();
     }
 
-    /**
-     * @fires
-     */
     onPause() {
         BirdSoundService.pause();
     }
 
-    /**
-     * @fires
-     */
     onResume() {
         BirdSoundService.success();
     }
 
-    /**
-     * @fires
-     */
     onUpdate() {
-        console.log(this.medail);
-
         WhipSoundService.play();
         BirdSoundService.success();
         this.notice.background = `items/medails/medail-${this.medail}.png`;
         this.notice.show(this.time);
     }
 
-    /**
-     * @event
-     */
     back() {
-        RouterComponent.navigate("square-list");
+        RouterComponent.navigate('square-list');
     }
 
 }
--- src/app/shared/services/score.service.js
+++ src/app/shared/services/score.service.js
@@ -2,38 +2,29 @@ import { Service } from 'babel-skeleton';
 
 export const ScoreService = new class extends Service {
 
-    /**
-     * @constructor
-     */
     constructor() {
         super();
         this.time = 0;
         this.state = null;
     }
 
-    /**
-     * @emits
-     */
     start() {
-        this.state = "start";
+        this.state = 'start';
     }
 
-    /**
-     * @emits
-     */
     stop() {
-        this.state = "stop";
+        this.state = 'stop';
         this.notify();
     }
 
-    /**
-     * @param {Square} square
-     * @returns {Number}
-     */
     medail(time) {
         return time
-            ? time > 175 ? "gold" : (time > 50 ? "silver" : "bronze")
-            : "black";
+            ? time > 175 
+                ? 'gold'
+                : (time > 50 
+                    ? 'silver' 
+                    : 'bronze')
+            : 'black';
     }
 
 }
